92deaab99df7f1146d527d8928c6de3f2764c043
IMarunasheva <i.marunasheva@mail.ru>
2025-01-04 18:27:15 +0300

    Добавьте файлы проекта.

---
+++ Module_13.6.1/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Module_13._6._1
+{
+    class Program
+    {
+        // объявим список в виде статической переменной
+        public static List<string> List = [];
+
+        // объявим связанный список в виде статической переменной
+        public static LinkedList<string> LinkedList = [];
+
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("input1.txt");
+
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = [' ', '\r', '\n'];
+
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            // Запустим первый таймер
+            var watchOne = Stopwatch.StartNew();
+
+            foreach (var word in words)
+            {
+                List.Add(word);
+            }
+
+            watchOne.Stop();
+
+            // Выведем результат первого таймера
+            Console.WriteLine($"Производительность вставки слов из текста в List: {watchOne.Elapsed.TotalMilliseconds}  мс");
+
+            // Запустим второй таймер
+            var watchTwo= Stopwatch.StartNew();
+
+            foreach (var word in words)
+            {
+                LinkedList.AddLast(word);
+            }
+
+            watchTwo.Stop();
+
+            // Выведем результат второго таймера
+            Console.WriteLine($"Производительность вставки слов из текста в LinkedList: {watchTwo.Elapsed.TotalMilliseconds}  мс");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Module_13.6.2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Module_13._6._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = File.ReadAllText("input2.txt");
+            
+            var noPunctuationText = new string(text.Where(c => !char.IsPunctuation(c)).ToArray());
+            
+            // Сохраняем символы-разделители в массив
+            char[] delimiters = [' ', '\r', '\n'];
+            
+            var words = noPunctuationText.Split(delimiters, StringSplitOptions.RemoveEmptyEntries);
+
+            var result = words.GroupBy(x => x)
+                              .Where(x => x.Count() > 1)
+                              .Select(x => new { Word = x.Key, Frequency = x.Count() });
+
+            Dictionary<string, int> DictWords = [];
+
+            foreach (var item in result)
+            {
+                DictWords.Add(item.Word, item.Frequency);
+            }
+
+            var orderedDictWords = DictWords.OrderByDescending(x => x.Value);
+
+            Console.WriteLine("Десять слов, которые встречаются чаще всего в тексте:");
+            Console.WriteLine();
+
+            int n = 1;
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine($"{n}. Cлово '{orderedDictWords.ElementAt(i).Key}' встречается {orderedDictWords.ElementAt(i).Value} раз");
+                n++;
+            }
+            
+            Console.ReadKey();
+        }
+    }
+}
